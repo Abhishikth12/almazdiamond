48c68b2e482af5fcd5c68669fcbb6c723c4fa5fc
veerugithubrit <veereshsveeru4@gmail.com>
2025-02-18 14:54:49 +0530

    a

--- diamond/Almazdiamond/Almazdiamondmo_app/urls.py
+++ diamond/Almazdiamond/Almazdiamondmo_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('test1',views.test_view, name='test'),
+    path('',views.test_view, name='test'),
     path('test2',views.test_2,name='test2'),
 ]
