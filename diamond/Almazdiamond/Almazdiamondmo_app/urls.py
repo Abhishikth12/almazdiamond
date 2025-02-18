022a843f4c80930dce54204d74ce34f6268c631d
veerugithubrit <veereshsveeru4@gmail.com>
2025-02-18 20:07:51 +0530

    Changes

--- diamond/Almazdiamond/Almazdiamondmo_app/urls.py
+++ diamond/Almazdiamond/Almazdiamondmo_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.test_view, name='test'),
+    path('',views.test_view, name='Homemain'),
     path('test2',views.test_2,name='test2'),
 ]
--- diamond/Almazdiamond/Almazdiamondmo_app/views.py
+++ diamond/Almazdiamond/Almazdiamondmo_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 # Create your views here.
 def test_view(request):
-    return render(request,'test.html')
+    return render(request,'Homemain.html')
 
 def test_2(request):
     return HttpResponse('working')
