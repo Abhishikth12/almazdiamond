97163ecb9cc04b31c70e3fb425525b19871ed322
Mouli Patibanda <moulipatibandavnsk@gmail.com>
2025-02-22 20:21:20 +0530

    changes

--- diamond/Almazdiamond/Almazdiamondmo_app/urls.py
+++ diamond/Almazdiamond/Almazdiamondmo_app/urls.py
@@ -5,13 +5,13 @@ from . import views
 urlpatterns=[
     path('',views.test_view, name='Homemain'),
     path('test2',views.test_2,name='test2'),
-
     path('admin_dashboard',views.admin_dashboard,name="admin_dashboard"),
-    path('submitproduct',views.submit_product,name="submitproduct"),
-    path('setting',views.setting,name="setting"),
-    path('stone',views.stone,name="stone"),
-
-
+    path('admin_dashboard/ring-settings',views.ring_settings_admin,name='ring_settings_admin'),
+    path('add-ring',views.add_edit_ring_settings,name='add_ring'),
+    path('edit-ring/<int:id>',views.add_edit_ring_settings,name='edit_ring'),
+    path('delete_ring_setting/<int:id>',views.delete_ring_setting,name='delete_ring_setting'),
+    path('delete_ring_files/<int:rs_id>/<int:id>',views.delete_ring_files,name='delete_files'),
+    path('delete_image/<int:rs_id>',views.delete_ring_files,name="delete_image"),
     path('login',views.login,name='login'),
 
 ]
