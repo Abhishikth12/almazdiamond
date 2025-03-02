53cb61d110624562e3a8dc8b4c6d96d1678aecee
Mouli Patibanda <moulipatibandavnsk@gmail.com>
2025-03-02 13:50:40 +0530

    aa

--- diamond/Almazdiamond/Almazdiamondmo_app/views.py
+++ diamond/Almazdiamond/Almazdiamondmo_app/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
-from .models import RingDetails, RingSettings
+from rest_framework.decorators import api_view
+from .serializers import *
+from rest_framework.response import Response
+import requests
+from django.db.models import Q
+from  Almazdiamond.settings import *
 #Create your views here.
 def test_view(request):
     return render(request,'Homemain.html')
@@ -20,57 +25,87 @@ def admin_dashboard(request):
 def ring_settings_admin(request):
     ring_settings=RingSettings.objects.all()
     context={'ring_settings':ring_settings}
-    return render(request,'ring_settings_admin.html',context)
+    return render(request,'ring_setting_admin.html',context)
 
 def add_edit_ring_settings(request,id=None):
-    ring_settings=ring_details=None
+    ring_settings=None
     if request.method=='POST':
         defaults={
-            "ring_settings":request.POST.get('ring_settings',''),
-            "metal_type":request.POST.get('metal_type',''),
-            "shapes":request.POST.get('shapes',''),
             "name":request.POST.get('name',''),
-            "description":request.POST.get('description',''),
-            "price":request.POST.get('price',0),
-            'currency':request.POST.get('currency','USD'),
-           
+            "description":request.POST.get('description','')
         }
-        if request.FILES.get('image'):
-            defaults["image"]=request.FILES.get('image')
+        
         ring,created=RingSettings.objects.update_or_create(
             id=id,
             defaults=defaults
         )
-        if ring:
-            files=request.FILES.getlist('files',[])
-            for i in files:
-                RingDetails.objects.create(
-                    ring=ring,
-                    file=i
-                )
-
         return redirect('ring_settings_admin')
     if id:
         ring_settings=RingSettings.objects.filter(id=id).first()
-        if ring_settings:
-            ring_details=RingDetails.objects.filter(ring=ring_settings)
-    return render(request,'add_edit_ring_setting.html',{"ring_settings":ring_settings,"setting_types":SETTING_TYPES,"metal_types":METAL_TYPES,"ring_shapes":RING_SHAPES,"currency":CURRENCY_CHOICES,"ring_details":ring_details,"id":id})
+        
+    return render(request,'add_edit_ring_setting.html',{"ring_settings":ring_settings,"setting_types":SETTING_TYPES,"metal_types":METAL_TYPES,"ring_shapes":RING_SHAPES,"currency":CURRENCY_CHOICES,"id":id})
+def ring_setting_variant_admin(request,ring_id):
+    ring_setting_variants=Ring_setting_variants.objects.filter(ring_id=ring_id)
+    ring_setting=RingSettings.objects.filter(id=ring_id).first()
+    print(ring_setting_variants,"ring_setting_variants")
+    return render(request,'ring_setting_variant_admin.html',{"ring_setting_variants":ring_setting_variants,"ring_setting":ring_setting})
+def add_edit_ring_setting_variant(request,ring_id,id=None):
+    ring_variant=ring_details=None
+    if request.method=='POST':
+        if ring_id:
+            defaults={
+            "price":request.POST.get('price',0),
+            'currency':request.POST.get('currency','USD'),
+            'ring_id':ring_id,
+            'ring_settings':request.POST.get('ring_settings',''),
+            'metal_type':request.POST.get('metal_type',''),
+            'shapes':request.POST.get('shapes','')
+            }
+            if request.FILES.get('image'):
+                defaults["image"]=request.FILES.get('image')
+            variant,created=Ring_setting_variants.objects.update_or_create(
+                    id=id,
+                    defaults=defaults
+
+            )
+            if variant:
+                files=request.FILES.getlist('files',[])
+                for i in files:
+                    RingDetails.objects.create(
+                        ring_variant=variant,
+                        file=i
+                    )
+
+        return redirect(reverse('ring_settings_variant_admin',kwargs={"ring_id":ring_id}))
+    if id:
+        ring_variant=Ring_setting_variants.objects.filter(id=id).first()
+        if ring_variant:
+            ring_details=RingDetails.objects.filter(ring_variant=ring_variant)
+
+    ring_setting=RingSettings.objects.filter(id=ring_id).first()
+    return render(request,'add_edit_ring_setting_variant.html',{"ring_setting":ring_setting,"ring_variant":ring_variant,"setting_types":SETTING_TYPES,"metal_types":METAL_TYPES,"ring_shapes":RING_SHAPES,"currency":CURRENCY_CHOICES,"ring_details":ring_details,"id":id})
 
 def delete_ring_setting(request,id):
     RingSettings.objects.filter(id=id).delete()
     return redirect('ring_settings_admin')
-
+def delete_ring_setting_variant(request,ring_id,id):
+    Ring_setting_variants.objects.filter(id=id).delete()
+    return redirect(reverse('ring_settings_variant_admin',kwargs={'ring_id':ring_id}))
 def delete_ring_files(reques,rs_id,id=None):
+    ring_id=None
     if rs_id and id:
-        ringdetails=RingDetails.objects.filter(ring_id=rs_id,id=id).first()
+        ringdetails=RingDetails.objects.filter(ring_variant=rs_id,id=id).first()
         if ringdetails:
-           ringdetails.delete()
+            ring_id=ringdetails.ring_variant.ring_id
+            ringdetails.delete()
     elif rs_id:
-        ring_settings=RingSettings.objects.filter(id=rs_id).first()
-        if ring_settings:
-            ring_settings.image=None
-            ring_settings.save()
-    return redirect(reverse('edit_ring',kwargs={"id":rs_id}))
+        ringsettingvariant=Ring_setting_variants.objects.filter(id=rs_id).first()
+        if ringsettingvariant:
+            ring_id=ringsettingvariant.ring_id
+
+            ringsettingvariant.image=None
+            ringsettingvariant.save()
+    return redirect(reverse('edit_ring_variant',kwargs={"ring_id":ring_id,"id":rs_id}))
 
 def stone_admin(request):
     stones=Stone.objects.all()
@@ -186,27 +221,93 @@ def delete_combination_files(request,c_id,id):
             combinationfile.delete()
     return redirect(reverse('edit_combination',kwargs={"id":c_id}))
 
+def ring_setting_api(request,varient_id=None):
+    api_url = f"{DomainName}api/ring_setting_filter_api"
+    params = {}
+    if request.GET.get('ring_settings'):
+        params['ring_settings'] = request.GET['ring_settings']
+    if request.GET.get('metal_type'):
+        params['metal_type'] = request.GET['metal_type']
+    if request.GET.get('shapes'):
+        params['shapes'] = request.GET['shapes']
+    if request.GET.get('price_min'):
+        params['price_min'] = request.GET['price_min']
+    if request.GET.get('price_max'):
+        params['price_max'] = request.GET['price_max']
+    if request.GET.get('currency'):
+        params['currency'] = request.GET['currency']
+    if varient_id:
+        params['varient_id']=varient_id
+
+    # Call the API
+    response = requests.get(api_url, params=params)
+    return response
 def ring_settings(request,stone_id=None):
-    ring_settings=RingSettings.objects.all()
-    return render(request,'setting.html',{"ring_settings":ring_settings,"settings":SETTING_TYPES,"stone_id":stone_id})
+    ring_variants=Ring_setting_variants.objects.all()
+    if 'ring_settings' in request.GET:
+        ring_variants=ring_variants.filter(ring_settings=request.GET['ring_settings'])
+    return render(request,'setting.html',{"ring_variants":ring_variants,"settings":SETTING_TYPES,"stone_id":stone_id})
 
-def get_more_ring_details(request, id, stone_id=None):
-    ring_details = RingDetails.objects.filter(ring_id=id)
-    ring = get_object_or_404(RingSettings, id=id)  # Fetch ring or return 404
+def get_more_ring_details(request, id=None, stone_id=None):
+    # ring = get_object_or_404(RingSettings, id=id)  # Fetch ring or return 404
+    # response=ring_setting_api(request,id)
+    # if response.status_code == 200:
+    #     ring_variants = response.json()  # API returns JSON response
+    #     ring_details = RingDetails.objects.filter(ring_variant_id=id)
 
+    #     print(ring_variants,"ring_variantsasasasa")
+    print(id,stone_id,";lllll")
+    selected_variant_ring=ring_variants=shapes=metal_types=setting_types=None
+    if id:
+        selected_variant_ring=Ring_setting_variants.objects.filter(id=id).first()
+        ring_variants=Ring_setting_variants.objects.filter(ring=selected_variant_ring.ring.id)
+        setting_types=ring_variants.values_list('ring_settings',flat=True).distinct()
+        metal_types=ring_variants.values_list('metal_type',flat=True).distinct()
+        shapes=ring_variants.values_list('shapes',flat=True).distinct()
+    else:
+        query=Q(ring_id=request.GET['ring_id'])
+        print(request.GET,"request.GET")
+        if 'style' in request.GET :
+            query&= Q(ring_settings=request.GET['style']) 
+            setting_types=Ring_setting_variants.objects.filter(ring_id=request.GET['ring_id']).values_list('ring_settings',flat=True).distinct()
+        elif 'material' in request.GET :
+            query&= Q(metal_type=request.GET['material']) 
+            metal_types=Ring_setting_variants.objects.filter(ring_id=request.GET['ring_id']).values_list('metal_type',flat=True).distinct()
+            print(metal_types,"klklk")
+        elif 'shapes' in request.GET :
+            print(request.GET['shapes'],"aaaaa")
+            query&= Q(shapes=request.GET['shapes']) 
+            shapes=Ring_setting_variants.objects.filter(ring_id=request.GET['ring_id']).values_list('shapes',flat=True).distinct()
+        
+        if query:
+            print(query,"cccc")
+            ring_variants=Ring_setting_variants.objects.filter(query)
+            if shapes:
+                setting_types=ring_variants.values_list('ring_settings',flat=True).distinct()
+                metal_types=ring_variants.values_list('metal_type',flat=True).distinct()
+            elif metal_types:
+                setting_types=ring_variants.values_list('ring_settings',flat=True).distinct()
+                shapes=ring_variants.values_list('shapes',flat=True).distinct()
+            elif setting_types:
+                metal_types=ring_variants.values_list('metal_type',flat=True).distinct()
+                shapes=ring_variants.values_list('shapes',flat=True).distinct()
+            print(ring_variants,"ring_variants")
+            selected_variant_ring=ring_variants.first()
+    
+   
+    ring_details=RingDetails.objects.filter(ring_variant=selected_variant_ring)
     return render(
         request,
         "ring_details.html",
         {
             "ring_details": ring_details,
-            "ring": ring,
-            "ring_shapes": RING_SHAPES,   # ✅ Already present
-            "metal_types": METAL_TYPES,   # ✅ Added this
-            "setting_types": SETTING_TYPES,  # ✅ Added this
-            "stone_id": stone_id,
+            "ring": selected_variant_ring,
+            "ring_shapes": shapes,   # ✅ Already present
+            "metal_types": metal_types,   # ✅ Added this
+            "setting_types": setting_types,  # ✅ Added this
+            "stone_id": stone_id
         },
     )
-
 def combination_stone_ring(request,stone_id,ring_id):
     combination=Combination.objects.filter(stone_id=stone_id,ring_id=ring_id).first()
     combination_details=CombinationFiles.objects.filter(combination_id=combination)
@@ -215,6 +316,18 @@ def combination_stone_ring(request,stone_id,ring_id):
 
 def stones(request,ring_id=None):
     stones=Stone.objects.all()
+    query=Q()
+    if 'stone_shape' in request.GET:
+        query&=Q(stone_shape__icontains=request.GET['stone_shape'])
+    if 'stone_clarity' in request.GET:
+        query&=Q(stone_clarity__icontains=request.GET['stone_clarity'])
+    if 'stone_cut' in request.GET:
+        query&=Q(stone_cut__icontains=request.GET['stone_cut'])
+    if query:
+        print(query,"query")
+        stones=Stone.objects.filter(query)
+        print(stones,"stones")
+
     return render(request,'stone.html',{"stones":stones,"ring_id":ring_id,"stone_shapes":STONE_SHAPES,"stone_cuts":STONE_CUT_CHOICES,"stone_clarity":STONE_CLARITY})
 
 def get_more_stone_details(request,id,ring_id=None):
@@ -222,3 +335,39 @@ def get_more_stone_details(request,id,ring_id=None):
     print(stone_details,"mmm")
     stone=Stone.objects.filter(id=id).first()
     return render(request,'stone_details.html',{"stone_details":stone_details,"stone":stone,"stone_shapes":STONE_SHAPES,"ring_id":ring_id})
+
+
+
+@api_view(['GET'])
+def ring_setting_filter_api(request):
+    query = Q()
+    varient_id=None
+    rings=serializer=None
+    if 'ring_settings' in request.GET:
+        query &= Q(ring_settings=request.GET['ring_settings', 'Solitaire'])
+    if 'metal_type' in request.GET:
+        query &= Q(metal_type=request.GET['metal_type','White Gold'])
+    if 'shapes' in request.GET:
+        query &= Q(shapes=request.GET['shapes','Round'])
+    if 'name' in request.GET:
+        query &= Q(name__icontains=request.GET['name'])  # Partial match
+    if 'description' in request.GET:
+        query &= Q(description__icontains=request.GET['description'])
+    if 'price_min' in request.GET:
+        query &= Q(price__gte=request.GET['price_min'])
+    if 'price_max' in request.GET:
+        query &= Q(price__lte=request.GET['price_max'])
+    if 'currency' in request.GET:
+        query &= Q(currency=request.GET['currency'])
+    if 'varient_id' in request.GET:
+        varient_id=request.GET['varient_id']
+    # Fetch results based on the filter
+    print(query,"lllll")
+    if query and varient_id:
+        rings = Ring_setting_variants.objects.filter(query,id=varient_id)
+        serializer = Ring_setting_variantsSerializer(rings, many=False)
+    else:
+        rings = Ring_setting_variants.objects.all()
+        serializer = Ring_setting_variantsSerializer(rings, many=True)
+
+    return Response(serializer.data)
